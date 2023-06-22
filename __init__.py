baa4c1e3712b5d4813e5a57218f7eca052fa0ab2
deto <43629311+detourious@users.noreply.github.com>
2023-06-22 11:35:35 -0700

    letter guess

--- __init__.py
+++ __init__.py
@@ -11,6 +11,14 @@ class Hangman(MycroftSkill):
         # pick a random word
         self.chosen_word = self.random_words[randint(0, len(self.random_words)-1)]
         self.speak_dialog('chosen_word', data={"length": len(self.chosen_word)})
+        response = self.get_response('guess_letter')
+        if not response is None:
+            if response[0:5] == "letter":
+                self.speak(f'valid letter {response[7:7]}')
+            else:
+                self.speak('invalid letter')
+        else:
+            self.speak('i dont know lol')
 
 def create_skill():
     return Hangman()
