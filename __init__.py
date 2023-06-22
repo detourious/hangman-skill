99bf4e6d2b3ffbe4d3acf387930655c66fed2a83
deto <43629311+detourious@users.noreply.github.com>
2023-06-22 11:23:24 -0700

    Pick a random word and say length

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from random import randint
 
 class Hangman(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.random_words = ["apple", "banana", "cherry"]
 
     @intent_file_handler('hangman.intent')
     def handle_hangman(self, message):
-        print(message)
-        self.speak_dialog('hangman')
-
+        # pick a random word
+        self.chosen_word = self.random_words[randint(0, len(self.random_words))]
+        self.speak_dialog('choosen_word', data={"length": len(self.chosen_word)})
 
 def create_skill():
     return Hangman()
