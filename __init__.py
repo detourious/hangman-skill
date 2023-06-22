6d00d896c255e4a0caa940dcd607f623c76a37a7
deto <43629311+detourious@users.noreply.github.com>
2023-06-22 12:29:16 -0700

    Letter guessing

--- __init__.py
+++ __init__.py
@@ -12,18 +12,30 @@ class Hangman(MycroftSkill):
         self.chosen_word = self.random_words[randint(0, len(self.random_words)-1)]
         self.lives_left = 6
         self.win_state = False
+        self.guessed_letters = []
         self.speak_dialog('chosen_word', data={"length": len(self.chosen_word)})
         
         while self.lives_left > 0 and not self.win_state:
             response = self.get_response('guess_letter', num_retries=0)
             if response is not None:
                 self.speak('you said ' + response)
+                
                 if self.voc_match(response, "valid_letters", None, True):
-                    letter_guess = response[5:8]
-                    self.speak("good")
-                    self.speak("the letter is " + letter_guess)
+                    letter_guess = response[7:7]
+                    self.speak("You guessed " + letter_guess)
+
+                    if self.chosen_word.find(letter_guess) > -1:
+                        self.speak("That is correct.")
+                    else:
+                        self.lives_left -= 1
+                        self.speak("That is incorrect. You now have " + str(self.lives_left) + " lives left.")
                 else:
                     self.speak("bad")
+
+        if self.win_state:
+            self.speak("You win!")
+        else:
+            self.speak("You lose!")
 
     def stop(self):
         self.lives_left = 0
